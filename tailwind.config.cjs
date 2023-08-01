db3cedd832a662988d9b74e7d170366e7bb4b9d3
Jon Granados <grxnxdos@gmail.com>
2023-07-31 21:58:56 -0700

    chore: setup Tailwind fontFamily

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'bayon': ['Bayon', 'sans-serif'], 
+			'montserrat': ['Montserrat', 'sans-serif'], 
+		}
 	},
 	plugins: [],
 }
