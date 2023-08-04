d4a401ee9b6e9bcc9a38581301eea31e5f04e189
Jon Granados <grxnxdos@gmail.com>
2023-08-03 19:43:58 -0700

    fix: tailwind css fontFamily definition syntax

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 		fontFamily: {
-			'bayon': ['Bayon', 'sans-serif'], 
-			'montserrat': ['Montserrat', 'sans-serif'], 
-		}
+			bayon: ["Bayon", "sans-serif"],
+			montserrat: ["Montserrat", "sans-serif"],
+		},
 	},
 	plugins: [],
-}
+};
