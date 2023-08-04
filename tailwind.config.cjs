e361826e1137c70ee89cfea7ae3a434928141740
Jon Granados <grxnxdos@gmail.com>
2023-08-04 11:22:02 -0700

    chore: define theme colors in tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				yellow: "#F7CF51",
+				grey: "#F2F2F2",
+				darkWave: "#53768A",
+				lightWave: "#8EC1DE",
+			},
+		},
 		fontFamily: {
 			bayon: ["Bayon", "sans-serif"],
 			montserrat: ["Montserrat", "sans-serif"],
