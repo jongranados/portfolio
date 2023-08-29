a7a0c54d181eef007fef5e64b87550ec107cbe24
Jon Granados <grxnxdos@gmail.com>
2023-08-28 21:05:17 -0700

    chore: add project thumbnail assets and append them to custom Tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,11 @@ module.exports = {
 		backgroundImage: {
 			placeholder: "url('./src/assets/placeholder.png')",
 			tenWords: "url('./src/assets/ten-words.jpeg')",
+			authenticateMe: "url('./src/assets/authenticate-me-project-bg.png')",
+			pixelPerfectInstacart: "url('./src/assets/pixel-perfect-instacart-project-bg.png')",
+			portfolioWebsite: "url('./src/assets/portfolio-website-project-bg.png')",
+			smartShoppingList: "url('./src/assets/smart-shopping-list-project-bg.png')",
+			socialPets: "url('./src/assets/social-pets-project-bg.png')",
 		},
 	},
 	plugins: [],
